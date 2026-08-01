f4b8e9a7cfc497a3ebb1b9661c15629a4ed11f61
Ibrahimmt11 <abitmulyo@gmail.com>
2026-08-01 07:47:31 +0700

    .

--- src/server.js
+++ src/server.js
@@ -1,14 +1,8 @@
-const express = require('express');
 require('dotenv').config();
-
-const app = express();
-app.use(express.json());
-
-app.get('/health', (req, res) => {
-  res.json({ status: 'ok' });
-});
+const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
