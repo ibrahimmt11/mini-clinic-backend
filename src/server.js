ff377d41b04854d27f477d527ff88278f3554724
Ibrahimmt11 <abitmulyo@gmail.com>
2026-08-01 08:27:21 +0700

    database connection

--- src/server.js
+++ src/server.js
@@ -3,6 +3,14 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
+const { Sequelize } = require('sequelize');
+const config = require('./config/config.js')[process.env.NODE_ENV || 'development'];
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+sequelize.authenticate()
+  .then(() => console.log('Database connected.'))
+  .catch((err) => console.error('DB connection failed:', err));
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
